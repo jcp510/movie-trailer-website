5ee829be1b8c7755a3e9211931a99a88336495be
jcp510 <jcponce510@gmail.com>
2018-07-12 19:24:52 -0700

    Added docstrings to class definition and function in media.py.

--- media.py
+++ media.py
@@ -2,13 +2,15 @@ import webbrowser
 
 # Data structure to store favorite movies.
 class Movie:
+    """"Class definition for creating instances of favorite movies."""
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
-        
+
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """Opens movie trailer.  Takes youtube trailer url as arg."""
         webbrowser.open(self.trailer_youtube_url)
